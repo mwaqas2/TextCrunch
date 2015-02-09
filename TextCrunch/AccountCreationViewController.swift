68145036840ba811c6d4c632fcf30017e5944b5f
George Coomber <georgecoomber@gmail.com>
2015-02-08 18:36:21 -0700

    Adding Login view controller and story board missed by previous commit

--- TextCrunch/AccountCreationViewController.swift
+++ TextCrunch/AccountCreationViewController.swift
@@ -10,6 +10,17 @@ import UIKit
 
 class AccountCreationViewController: UIViewController {
 
+	// Text fields for user account creation screen
+	@IBOutlet weak var emailTextField: UITextField!
+	@IBOutlet weak var passwordTextField: UITextField!
+	
+	// Handles the Create Account button press event.
+	@IBAction func createAccountButtonPressed(sender: AnyObject) {
+		if isValidEmail() && isValidPassword() {
+			self.performSegueWithIdentifier("AccountCreationSuccessful", sender: nil)			
+		}
+	}
+	
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -20,16 +31,29 @@ class AccountCreationViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+	
+	// Checks if the User has entered an email and if the email is valid.
+	// Returns true if the email is valid.
+	func isValidEmail() -> Bool {
+		if emailTextField.text.isEmpty {
+			return false
+		}
+		
+		// TODO: Check if the entered email is a valid email, and if the 
+		// and it is a new email address (not already in the parse database)
+		return true
+	}
+	
+	// Checks if the User has entered a password and if the password matches
+	// the password linked to the given email. Returns true if password is correct.
+	func isValidPassword() -> Bool {
+		if passwordTextField.text.isEmpty {
+			return false
+		}
+		
+		// TODO: Check if the given password contains valid characters
+		// and is the correct length
+		return true
+	}
 }
--- TextCrunch/LoginViewController.swift
+++ TextCrunch/LoginViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  LoginViewController.swift
 //  TextCrunch
 //
 //  Created by Derek Dowling on 2015-01-12.
@@ -8,8 +8,14 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class LoginViewController: UIViewController {
+	// Text fields for user login screen
+	@IBOutlet weak var emailTextField: UITextField!
+	@IBOutlet weak var passwordTextField: UITextField!
+	
+	let testEmail = "test@gmail.com"
+	let testPassword = "password"
+	
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -23,6 +29,49 @@ class ViewController: UIViewController {
 	override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
 		// Get the new view controller using segue.destinationViewController.
 		// Pass the selected object to the new view controller.
+	}
+	
+	// Handles the Login button press event.
+	@IBAction func loginButtonPressed(sender: AnyObject) {
+		if loginUser() {
+			self.performSegueWithIdentifier("Login",sender: nil)
+		}
+	}
+	
+	// Handles the Create Account button press event.
+	@IBAction func createAccountButtonPressed(sender: AnyObject) {
+		self.performSegueWithIdentifier("CreateAccount", sender: nil)
+	}
+	
+	// Checks if the User has entered an email and if the email is valid.
+	// Returns true if the email is valid.
+	func isValidEmail() -> Bool {
+		if emailTextField.text.isEmpty {
+			return false
+		}
+		
+		// TODO: compare input email to actual emails from Parse
+		return (emailTextField.text == testEmail)
+	}
+	
+	// Checks if the User has entered a password and if the password matches
+	// the password linked to the given email. Returns true if password is correct.
+	func isCorrectPassword() -> Bool {
+		if passwordTextField.text.isEmpty {
+			return false
+		}
+		
+		//TODO: compare input email to actual emails from Parse
+		return (passwordTextField.text == testPassword)
+	}
+	
+	// Attempts to login the User in, with the given email and password
+	// returns true if the User has been successfully logged in
+	// returns false if the login was unsuccessful
+	func loginUser() -> Bool {
+		//TODO: Add user login here
+		
+		return isValidEmail() && isCorrectPassword()
 	}
 }
 
