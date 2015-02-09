a5d68651a4df6f01bb6cb6d82c6d9d79cd05a6fb
George Coomber <georgecoomber@gmail.com>
2015-02-08 18:29:25 -0700

    Adding Views and ViewControllers for Login and Account creation

---
+++ TextCrunch/AccountCreationViewController.swift
@@ -0,0 +1,35 @@
+//
+//  AccountCreationViewController.swift
+//  TextCrunch
+//
+//  Created by George Coomber on 2015-02-05.
+//  Copyright (c) 2015 Text Crunch. All rights reserved.
+//
+
+import UIKit
+
+class AccountCreationViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
