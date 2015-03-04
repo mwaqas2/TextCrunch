f155123ab09e4191f4c6c35a23ecbd1a1646bdc0
George Coomber <georgecoomber@gmail.com>
2015-03-04 12:23:07 -0700

    adding segue for chat

--- TextCrunch/ChatViewController.swift
+++ TextCrunch/ChatViewController.swift
@@ -9,7 +9,22 @@
 // It will then check database if a conversation already exists for that listing and this buyer
 // If not it will create a new conversation
 
+import UIKit
 import Foundation
+
+class ChatViewController : UIViewController {
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		
+		// Do any additional setup after loading the view.
+	}
+	
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+}
+
 /*
 class ChatViewController : UIViewController, UITableViewDataSource, UITableViewDelegate {
 	@IBOutlet var messagesTableView: UITableView!
